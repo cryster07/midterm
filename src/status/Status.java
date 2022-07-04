642f11b05df4a8619a42ff01d5d6af187d7f128c
cryster07 <ghimirecryster@gmail.com>
2022-07-04 12:31:21 -0400

    program

--- src/status/Status.java
+++ src/status/Status.java
@@ -7,27 +7,33 @@ package status;
 
 import java.util.Scanner;
 
-/** This class takes String input from user 
- * and calls method statusDetail to print the 
- * status of User-Rejected,Pending,Processing and Approved.
- * if the input is not given in String, 
- * program will accept and produce wrong results. 
- * change the code to use enums which avoids String input 
- * then print the status details. 
- * @author srinivsi 
+/**
+ * This class takes String input from user and calls method statusDetail to
+ * print the status of User-Rejected,Pending,Processing and Approved. if the
+ * input is not given in String, program will accept and produce wrong results.
+ * change the code to use enums which avoids String input then print the status
+ * details.
+ *
+ * @author srinivsi
  */
 public class Status {
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args)
-    {
-    Scanner in =new Scanner(System.in);
-    System.out.println("Enter the user status code (zero,one,two,three) in string");
-    String code = in.next();
-    StausUser t= new StausUser();
-    t.statusDetail(code); 
+    public enum Code {
+        ZERO, ONE, TWO, THREE};
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the user status code (zero,one,two,three) in string");
+
+        String s = in.next();
+        Code code = Code.valueOf(s);
+
+        StausUser t = new StausUser();
+        t.statusDetail(code.valueOf(s));
+        in.close();
     }
-    
+
 }
